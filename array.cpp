0fd5e5ba8bdedee5eb0698e902793074f046eeca
SHASHANK RAI <shashank.rai00331@gmail.com>
2021-05-04 21:56:28 +0530

    changes in arrays

--- array.cpp
+++ array.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char const *argv[])
+int main()
 {
     //////////////////////////// checking for garbage values in for each loop
 
@@ -12,20 +12,9 @@ int main(int argc, char const *argv[])
     //     cout << x<<endl;
     // }
 
-
-
-
-
-
-
-
-
-    ///////////////////////////// taking float array and integer in for each loop 
-    // it typecasts and gives out results in integer form 
-    // for float it gives in float 
-
-
-
+    ///////////////////////////// taking float array and integer in for each loop
+    // it typecasts and gives out results in integer form
+    // for float it gives in float
 
     // float A[3] = {3.55, 3.65f, 2.3f};
 
@@ -34,17 +23,7 @@ int main(int argc, char const *argv[])
     //     cout << x<<endl;
     // }
 
-
-
-
-
-
-
-
-
-
     //////////////////////////// checking typecasting for char
-
 
     // float A[3] = {3.55, 3.65f, 2.3f};
 
@@ -52,11 +31,6 @@ int main(int argc, char const *argv[])
     // {
     //     cout << x<<endl;
     // }
-
-
-
-
-
 
     //////////////////////////// use of auto in foreach loop
 
@@ -67,24 +41,47 @@ int main(int argc, char const *argv[])
     //     cout << x<<endl;
     // }
 
-
-
-
-
     //////////////////////////////// using characters in for each loop
     //////// learn about ascii codes for 'hex'  and 'char'
 
+    // char A[4] = {'A',66,44,'B'};
 
-    char A[4] = {'A',66,44,'B'};
+    // for (auto x : A)
+    // {
+    //     cout << x<<endl;
+    // }
 
-    for (auto x : A)
+    /////////////////////////////////// use of INT_MIN , INT_MAX
+    //////////// finding max and minimum number
+
+    // int A[4] = {1, 2, 3, 4};
+    // int max = INT_MIN;
+
+    // for (auto x : A)
+    // {
+    //     if (x > max)
+    //     {
+    //         max = x;
+    //     }
+    // }
+
+    // cout << max << endl;
+
+    /////////////////////////   WRITE PROGRAM TO FIND INDEX OF ELEMENTS IN AN ARRAY
+
+    //////////////////////////   MAKING A  2D ARRAY USING NESTED LOOPS
+
+    for (int i = 0; i < 3; i++)
     {
-        cout << x<<endl;
+        for (int j = 0; j < 3; j++)
+        {
+            //cout << '*';
+            cout<<i<<j<<endl;
+        }
+        cout << endl;
     }
 
-
-
-
+    //////////////////////////////
 
     return 0;
 }
