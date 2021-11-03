72caebb00415c1d65c418fb95d8c4069154ebee6
shashank rai <shashank.rai00331@gmail.com>
2021-11-03 14:50:45 +0530

    svfv

--- d-struct/arrays.cpp
+++ d-struct/arrays.cpp
@@ -16,6 +16,8 @@
 
 
 
+// ------------------------------------------   code 1-------------------------------------
+
 
 // class Solution {
 // public:
@@ -39,7 +41,7 @@
 //             }
 //             if(c==1)
 //                 break;
-//         }
+//         
 //         return v;
 //     }
 // };
@@ -48,53 +50,90 @@
 
 
 
+// --------------------------------------  code 2 ----------------------------------
 
 
 
 
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+        
+        
+//         int n=nums.size();
+//     vector<pair<int,int>> v;
+        
+//         for(int i=0;i<n;i++){
+//             v.push_back({nums[i],i});
+//         }
+        
+//    sort(v.begin(),v.end());
+  
+//        int i=0,j=n-1;
+        
+//         while(i<j){
+//             int sum=v[i].first+v[j].first;
+//             if(sum==target){
+//                 return {v[i].second,v[j].second};
+//             }
+//             else if(sum<target){
+//                 i++;
+                
+//             }
+//             else{
+//                 j--;
+//             }
+//         }
+//         return {-1,-1};
+    
+        
+        
+   
+//     }
+    
+       
+// };
+   
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// --------------------------------------   my-code ---------------------------------- 
 
 #include<iostream>
 using namespace std;
 int main()
 {
-   int A[5], b,i,j;
-   cin>>A[5];
-   cin>>b;
-   
-   for (int i = 0; i <4; i++)
-   {
-       for (int j = 0; i <4; i++)
-       {
-           cout<<j;
-           
-       }
+   int A[5] , B[2] , target;
 
-       cout<<i;
+   for (int i = 0; i <5; i++)
+   {       
+    cin>>A[i];     
+   }
+   cout<<"enter target value";
+   cin>>target;
+
+
+   for (int i = 0; i < 5; i++)
+   {
+      for (int j = i+1; j < 6; j++)
+      { 
+
+         if (A[i]+A[j] == target) {
+
+            cout<<i<<j;
+
+                 
+         }
+         
        
+      }
+      
    }
    
    
-    return 0;
+
+   
+   
+   return 0;
 }
+
